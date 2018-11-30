e2614d1ccf7e7bc2e6f03afd7725a6c4684bd771
David Snyder <davidsnyder@loki.local>
2018-11-30 14:56:23 -0600

    Basic tic-tac-toe implementation

--- src/index.js
+++ src/index.js
@@ -2,23 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
+function Square(props) {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+      className="square"
+      onClick={props.onClick}
+      >
+        {props.value}
       </button>
     );
-  }
+}
+
+function ResetButton(props) {
+    return (
+      <button
+      className="reset"
+      onClick={props.onClick}
+      >
+        Reset Board
+      </button>
+    );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+    <Square
+    value = {this.state.squares[i]}
+    onClick = {() => this.handleClick(i)}
+    />
+    );
+  }
+
+  renderResetButton() {
+    return (
+      <ResetButton
+      onClick = {() => this.resetBoard()}
+      />
+      );
+  }
+
+  resetBoard() {
+    this.setState({squares: Array(9).fill(null), xIsNext: true});
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    const tieGame = allFilled(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else if (tieGame) {
+      status = "Tie Game";
+    } else {
+      status = (this.state.xIsNext ? 'X' : 'O') + "'s Turn:";
+    }
+
 
     return (
       <div>
@@ -38,6 +97,7 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        {this.renderResetButton()}
       </div>
     );
   }
@@ -57,6 +117,30 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i=0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function allFilled(squares) {
+  return squares.every( i => i != null);
 }
 
 // ========================================

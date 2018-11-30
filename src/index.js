5faedd8c636c6765b61d013a55a5cf47cc255e29
David Snyder <davidsnyder@loki.local>
2018-11-30 14:57:16 -0600

    Fix indentation in src/index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-    return (
-      <button
-      className="square"
-      onClick={props.onClick}
-      >
-        {props.value}
-      </button>
+  return (
+    <button
+    className="square"
+    onClick={props.onClick}
+    >
+    {props.value}
+    </button>
     );
 }
 
 function ResetButton(props) {
-    return (
-      <button
-      className="reset"
-      onClick={props.onClick}
-      >
-        Reset Board
-      </button>
+  return (
+    <button
+    className="reset"
+    onClick={props.onClick}
+    >
+    Reset Board
+    </button>
     );
 }
 
@@ -47,11 +47,11 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-    <Square
-    value = {this.state.squares[i]}
-    onClick = {() => this.handleClick(i)}
-    />
-    );
+      <Square
+      value = {this.state.squares[i]}
+      onClick = {() => this.handleClick(i)}
+      />
+      );
   }
 
   renderResetButton() {
@@ -81,25 +81,25 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-        {this.renderResetButton()}
+      <div className="status">{status}</div>
+      <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
       </div>
-    );
+      <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+      </div>
+      {this.renderResetButton()}
+      </div>
+      );
   }
 }
 
@@ -107,28 +107,28 @@ class Game extends React.Component {
   render() {
     return (
       <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div className="game-board">
+      <Board />
       </div>
-    );
+      <div className="game-info">
+    <div>{/* status */}</div>
+  <ol>{/* TODO */}</ol>
+  </div>
+  </div>
+  );
   }
 }
 
 function calculateWinner(squares) {
-    const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
   ];
   for(let i=0; i < lines.length; i++) {
     const [a,b,c] = lines[i];
@@ -148,4 +148,4 @@ function allFilled(squares) {
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
-);
+  );
